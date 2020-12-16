66f9010abf65a5aa51782fa4a633c89dd3548122
Daniel Figueroa <dfigueroa7007@gmail.com>
2020-12-15 17:49:25 -0800

    Added routes to js file and function to view all employees

--- eeTracker.js
+++ eeTracker.js
@@ -15,8 +15,77 @@ var connection = mysql.createConnection({
       console.error("Error connecting: " + err.stack);
       return;
     }
-
     console.log("Connected as id: " + connection.threadId);
+    runTrackerChoices()
   });
 
-  
+function runTrackerChoices() {
+  inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "userCommand",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add department",
+        "Add role",
+        "Add employee",
+        "Update employee role",
+        "Exit"
+      ]
+    },
+  ])
+  .then(answers => {
+    console.log(answers)
+    switch (answers.userCommand) {
+      case "View all departments":
+        viewDept();
+        break;
+
+      case "View all roles":
+        viewRoles();
+        break;
+
+      case "View all emlpoyees":
+        viewAllEmp();
+        break;
+
+      case "Add department":
+        addDep();
+        break;
+
+      case "Add role":
+        addRole();
+        break;
+
+      case "Add employee":
+        addEmp();
+        break;
+
+      case "Update employee role":
+        udpateEmpRole();
+        break;
+
+      case "Exit":
+        connection.end();
+        break;
+    }
+  })
+  .catch(error => {
+    console.log(error)
+  });
+}
+
+function viewDept() {
+  var query = "SELECT * FROM ee_tracker_db.department";
+  connection.query(query, function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(res);
+    }
+    runTrackerChoices()
+  });
+}
